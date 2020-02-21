89c1ff12fc7c5b4b5d43cbb61817965cf0d62bf7
bladrome <blackwhitedoggie@163.com>
2020-02-21 10:00:06 +0800

    Termux config

---
+++ termux/config.sh
@@ -0,0 +1,16 @@
+
+termux-setup-storage
+
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux stable main@' $PREFIX/etc/apt/sources.list
+pkg update && pkg upgrade -y
+
+pkg install -y openssh git zsh wget proot tmux
+chsh -s zsh
+sshd
+
+
+git clone https://gitee.com/lu_x/ohmyzsh.git .oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s $(which zsh)
+
+passwd `whoami`
