0a216ead7b5c924986e4a7c8c847706b027b845e
bladrome <blackwhitedoggie@163.com>
2021-06-22 20:52:56 +0800

    update conda reenv

---
+++ conda/reinstall.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2021.05-Linux-x86_64.sh
+# bash Anaconda3-2021.05-Linux-x86_64.sh
+
+
+source `dirname $CONDA_EXE`/../etc/profile.d/conda.sh
+
+if test ! -e envs; then mkdir envs; fi
+
+conda env list | awk '{print $1}' | grep -v "#" | grep -v ^$ | while read myenv
+do
+
+    conda activate $myenv;
+    conda env export -f envs/$myenv.yaml
+    conda deactivate
+done
+
+
+for myenv in *yaml
+do
+    echo $myenv
+    conda envv create -f $myenv
+done
