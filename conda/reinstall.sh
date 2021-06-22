e9d06f0222574cbcf8f5f11b5fd530301aa96846
bladrome <blackwhitedoggie@163.com>
2021-06-22 21:14:23 +0800

    reinstall anaconda

--- conda/reinstall.sh
+++ conda/reinstall.sh
@@ -3,6 +3,7 @@
 source `dirname $CONDA_EXE`/../etc/profile.d/conda.sh
 
 condadir=`dirname $CONDA_EXE`
+condadir=`dirname $condadir`
 if test ! -e envs; then mkdir envs; fi
 
 conda env list | awk '{print $1}' | grep -v "#" | grep -v ^$ | while read myenv
@@ -13,10 +14,10 @@ do
     conda deactivate
 done
 
-rm -rf `dirname $condadir`
+rm -rf $condadir
 
 wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2021.05-Linux-x86_64.sh
-bash Anaconda3-2021.05-Linux-x86_64.sh
+bash Anaconda3-2021.05-Linux-x86_64.sh -p $condadir -b
 
 for myenv in *yaml
 do
