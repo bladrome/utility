daa7e36d91531ee0a297d0df8329c88862ed2b63
blkcat <blackwhitedoggie@163.com>
2021-07-02 09:42:51 +0800

    Update gputop.sh

--- scripts/gputop.sh
+++ scripts/gputop.sh
@@ -5,12 +5,12 @@ trap 'rm -f "$TMPFILE"' EXIT
 TMPFILE=$(mktemp) || exit 1
 
 for pid in $(nvidia-smi | grep -A 100 PID | tail -n +4 | grep '|' | awk '{print $5}'); do
-    ps axo user:20,pid,pcpu,pmem,vsz,rss,tty,stat,start,time,comm | grep $pid | grep -v $USER | awk '{print $1 "\t" $2}' >> $TMPFILE
+    ps hao user:20,pid -q $pid  >> $TMPFILE
 done
 
 nvidia-smi
 echo -e "\nuser name and pid in nvidia-smi:"
-cat $TMPFILE
+awk '{printf "%10s\t%-10s\n", $1, $2}' $TMPFILE
 echo -e "\nuser statistic:"
-cut -d $'\t' -f 1 $TMPFILE | sort | uniq -c | sort -r
+awk '{print $1}' $TMPFILE | sort | uniq -c | sort -r
 echo 
